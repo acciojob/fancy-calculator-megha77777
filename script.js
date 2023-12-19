9c7a4eca51df62619b7cf77ed23ccd64832a2e43
megha77777 <141833704+megha77777@users.noreply.github.com>
2023-12-19 18:55:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let a = prompt("Enter a number");
+a = Number.parseInt(a);
+let b = prompt("Enter another number");
+b = Number.parseInt(b);
+a = a*2;
+let result = a+b;
+alert("The final result is:"+result)
